2a749e9ac69bf3ea2725e957544e1840bbdb7b98
Alexander Kuzin-Aleksinskij <alexander.k-aleksinskij@ya.ru>
2019-05-16 17:57:23 +1000

    улучшение npm-скриптов
    Так же, мелкие правки в коде api-сервера.

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -1,15 +1,17 @@
 import * as Koa from 'koa';
 import * as indexRoutes from './routes/index';
 import * as usersRoutes from './routes/users';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const dotEnvConfigPath = path.resolve(__dirname, ".env");
+dotenv.config({ path: dotEnvConfigPath })
+const dbUri = process.env["DB_URI"];
 
 const app = new Koa();
 
 // подключаем главные маршрутизаторы
-app.use(indexRoutes.routes);
-
-// подключаем главные маршрутизаторы
 app.use(usersRoutes.routes);
-
 
 // подключаем маршрутизаторы users
 const application = app.callback();
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,6 @@ import { Server } from 'http';
 import application from './app/app';
 
 const server = new Server(application);
-
 server.listen(3000, "localhost", () => {
     const address = server.address();
     console.log(`Server has started at ${address.address}:${address.port}`);
